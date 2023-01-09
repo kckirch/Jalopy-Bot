fbeafc132a9a95b352d67c0e9e52767aa1b0c670
kckirch <kckircher@hotmail.com>
2023-01-09 15:26:20 -0700

    works but needs show all models fixed

--- responses.py
+++ responses.py
@@ -12,57 +12,93 @@ def get_response(message: str) -> str:
     if p_message == '!help':
         return '`Welcome to the Jalopy Jungle Bot \n \nI am being built to help notify users for new car inventory. \n`'
 
-    if p_message == '!s':
-        # Set up the web driver
-        driver = webdriver.Chrome()
 
-        # Navigate to the website
-        driver.get("http://inventory.pickapartjalopyjungle.com/")
 
-        # Execute the JavaScript code
-        driver.execute_script("$('#yard-id').val('1020');")
-        driver.execute_script("$('#car-make').val('BMW');")
-        driver.execute_script("$('#car-model').val('3 SERIES');")
-        driver.execute_script("$('#car-model').change();")
 
-        # Get the response from the website
-        response = driver.page_source
+    location_to_yard_id = {
+    'BOISE': 1020,
+    'CALDWELL': 1021,
+    'GARDENCITY': 1119,
+    'NAMPA': 1022,
+    'TWINFALLS': 1099,
 
-        # Parse the HTML response
-        soup = BeautifulSoup(response, 'html.parser')
+}    
 
-        # Find the table element
-        table = soup.find('table', class_='table')
+    if p_message.startswith('!s'):
+        try:
+            # Split the message by ',' and extract the arguments
+            args = message.split(' ')
+            location = args[1].upper().strip(' ')
+            car_make = args[2].upper().strip()
+            car_model = ' '.join(args[3:]) if len(args) > 3 else 'Select Model'
 
-        # Find all the rows in the table
-        rows = table.find_all('tr')
+            #testing
+            print(location)
+            print(car_make)
+            print(car_model)
 
-        # Initialize a list of lists to store the table data
-        table_data = []
+            # Convert the location name to a yard-id value
+            try:
+                yard_id = location_to_yard_id[location]
+            except KeyError:
+                return f"I'm sorry, I don't recognize the location '{location}'. Please try again with a valid location."
 
-        # Iterate over the rows
-        for row in rows:
-            # Find all the cells in the row
-            cells = row.find_all('td')
+            # Set up the web driver
+            driver = webdriver.Chrome()
 
-            # Check if the row has at least 4 cells
-            if len(cells) >= 4:
-                # Add the row data to the table data list
-                table_data.append([cells[0].text, cells[1].text, cells[2].text, cells[3].text])
+            # Navigate to the website
+            driver.get("http://inventory.pickapartjalopyjungle.com/")
 
-        # Find the row names
-        row_names = [row.text for row in rows[0].find_all('th')]
+            # Execute the JavaScript code
+            driver.execute_script(f"$('#yard-id').val('{yard_id}');")
+            driver.execute_script(f"$('#car-make').val('{car_make}');")
 
-        # Import the tabulate library
-        from tabulate import tabulate
+            driver.execute_script("$('#car-make').change();")
+            
+            driver.execute_script(f"$('#car-model').val('{car_model}');")
+            driver.execute_script("$('#car-model').change();")
 
-        # Generate the formatted table
-        formatted_table = tabulate(table_data, headers=row_names, tablefmt='rounded_outline', stralign= 'right' , numalign= ['left','right'])
 
-        # Close the web driver
-        driver.close()
+            # Get the response from the website
+            response = driver.page_source
 
-        # Return the formatted table
-        return "`" + formatted_table + "`\n" + current_time
+            # Parse the HTML response
+            soup = BeautifulSoup(response, 'html.parser')
+
+            # Find the table element
+            table = soup.find('table', class_='table')
+
+            # Find all the rows in the table
+            rows = table.find_all('tr')
+
+            # Initialize a list of lists to store the table data
+            table_data = []
+
+            # Iterate over the rows
+            for row in rows:
+                # Find all the cells in the row
+                cells = row.find_all('td')
+
+                # Check if the row has at least 4 cells
+                if len(cells) >= 4:
+                    # Add the row data to the table data list
+                    table_data.append([cells[0].text, cells[1].text, cells[2].text, cells[3].text])
+
+            # Find the row names
+            row_names = [row.text for row in rows[0].find_all('th')]
+
+            # Import the tabulate library
+            from tabulate import tabulate
+
+            # Generate the formatted table
+            formatted_table = tabulate(table_data, headers=row_names, tablefmt='rounded_outline', stralign= 'right' , numalign= ['left','right'])
+
+            # Close the web driver
+            driver.close()
+
+            # Return the formatted table
+            return "`" + formatted_table + "`\n" + current_time
+        except Exception as e:
+            return f'An error occurred: {e}'
 
     return 'I didn\'t understand what you wrote. Try typing "!help".'
